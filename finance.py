f827557fb261091c7050f40931e7060d50e9ac2b
pikkake <56040139+pikkake@users.noreply.github.com>
2019-12-16 15:22:33 -0600

    Help added
    Added the foundation of a help function so I don't lose track of everything lol

--- finance.py
+++ finance.py
@@ -13,40 +13,45 @@ budget_raw = {
       {"Groceries":[150, 57]}
       ]
 }
-
-budget_debug = Budget()
-budget_debug.printBudget()  
-while True:
-  budget = Budget()
-  args = []
-  stdin = versionless_input(">> ").strip()
-  
-  #uses a function that checks py version to call the built-in input function
-  try:
-    args = stdin.split()
-  except:
-    args.append(stdin)
-  
-  try:
-    if stdin == "exit" or stdin == "e":
-      print("Goodbye")
-            
-      break
-    #elif stdin == "start budget":
-      #writeBudget(budget_raw)
-    elif stdin == "print":
-      budget.printBudget()
-    elif args[0] == "-v" and len(args) < 2:
-      fetchPythonVersion("list")
-    elif args[0] == 'alt':
-      budget.alter_Item(args)
-    elif args[0] == 'add':
-      budget.add_Item(args)
-    elif args[0] == 'del':
-        budget.delete_Item(args)
-    elif args[0] == "help":
-      help(Budget.delete_Item)
-  except IndexError:
+budget = Budget()
+def run(budget):
+  while True:
+    
     budget.printBudget()
-	
-closePy()
+    budget.passedMsg = ""
+    #uses a function that checks py version to call the built-in input function
+    try:
+      args = []
+      stdin = versionless_input(">> ").strip()
+      args = stdin.split()
+    except IndexError:
+      args.append(stdin)
+    except EOFError:
+      break
+    
+    try:
+      if stdin == "exit" or stdin == "e":
+        print("Goodbye")              
+        break
+      #elif stdin == "start budget":
+        #writeBudget(budget_raw)
+      elif stdin == "print":
+        budget.printBudget()
+      elif args[0] == "-v" and len(args) < 2:
+        clear()
+        fetchPythonVersion("list")
+        versionless_input("\nPress ENTER to return...")
+      elif args[0] == 'alt':
+        budget.alter_Item(args)
+      elif args[0] == 'add':
+        budget.add_Item(args)
+      elif args[0] == 'del':
+          budget.delete_Item(args)
+      elif args[0] == "help":
+        budget.budgetHelp(args)
+    except IndexError:
+      continue
+  	
+  closePy()  
+  
+run(budget)
